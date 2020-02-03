4dfdb91f0f051e6f0876cee7d578de4cd676f7fe
Alexander Potukar <alexander.potukar@eon.com>
2020-02-03 13:29:38 +0100

    devto adoptions

--- devto.sh
+++ devto.sh
@@ -2,4 +2,5 @@
 
 project=$(basename "$PWD")
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-sed "s/\.\/images/https:\/\/raw.githubusercontent.com\/eon-com\/$project\/$branch\/images/" README.md | tail -n +3 > README_DEVTO.md
+timestamp=$(date +%s)
+sed "s/\.\/images/https:\/\/raw.githubusercontent.com\/eon-com\/$project\/$branch\/images/" README.md | sed "s/\.png/\.png?version=$timestamp/" | tail -n +3 | pbcopy
